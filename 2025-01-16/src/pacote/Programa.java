aa7f0d0f588a10ea38d769b4d8102ab32fa91216
Caio de Matos Vital <81044580+caiomvital@users.noreply.github.com>
2025-01-23 19:05:06 -0300

    Add files via upload

---
+++ 2025-01-16/src/pacote/Programa.java
@@ -0,0 +1,57 @@
+package pacote;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		//tipo logico
+		int valor = 2;
+		boolean valorPar = valor % 2 == 0; 
+		// >
+		// <
+		// >=
+		// <=
+		// !=
+		System.out.println(valor + " é par? " + valorPar);
+		int idade = 18;
+		boolean maiorDeIdade = idade >= 18;
+		System.out.printf("Maior de idade? %b%n", maiorDeIdade);		
+		System.out.println(valor + " é par? " + valorPar);
+		
+		
+		
+		
+//		int valor1 = 7, valor2 = 3;
+//		int soma = valor1 + valor2;
+//		int subt = valor1 - valor2;
+//		int mult = valor1 * valor2;
+//		double div = (double) valor1 / valor2;		
+//		
+//		
+//		// + soma
+//		System.out.println("A soma entre " + valor1 + 
+//				" e " + valor2 + " é " + soma);
+//		// - subtracao
+//		
+//		System.out.println("1" + 1);
+//		// * multiplicacao
+//		System.out.println(1 + 1);
+//		// / divisao
+//		System.out.println("1 + 1");
+		
+		// %
+		
+		
+		
+//		tipo textual
+//		String nomeDoCliente = "Gervásio";
+//		
+//		System.out.println(nomeDoCliente);
+//		System.out.println(nomeDoCliente);
+//		System.out.println(nomeDoCliente);
+//		System.out.println(nomeDoCliente);
+//		System.out.println(nomeDoCliente);
+		
+	}
+
+}
